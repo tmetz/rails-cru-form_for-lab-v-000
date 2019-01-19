7cc4973b09b767a7b61baac186b0636b5ab928c1
Tammy Metz <tammy@tammymetz.com>
2019-01-19 19:19:30 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,29 @@
 class GenresController < ApplicationController
-  def create
-  end
+  
+    def new
 
-  def show
-  end
+    end
+
+    def create
+      @genre = Genre.create(genre_params)
+      redirect_to genre_path(@genre)
+    end
+
+    def edit
+      @genre = Genre.find(params[:id])
+    end
+
+    def update
+      @genre = Genre.find(params[:id])
+      @genre.update(name: params[:genre][:name])
+      redirect_to genre_path(@genre)
+    end
+
+    def show
+      @genre = Genre.find(params[:id])
+    end
+
+    def genre_params
+      params.require(:name)
+    end
 end
